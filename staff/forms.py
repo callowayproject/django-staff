5e11911ed8aa3c1da5ef8a9ade2f25966fc313ef
Corey Oordt <coordt@washingtontimes.com>
2010-09-10 11:02:56 -0400

    Refactored a few settings to work better with current standards.

--- staff/forms.py
+++ staff/forms.py
@@ -4,20 +4,29 @@ from django.conf import settings
 
 from models import StaffMember
 
-if 'tinymce' in settings.INSTALLED_APPS:
+USE_TINYMCE = getattr(settings, 'STAFF_USE_TINYMCE', True)
+if 'tinymce' in settings.INSTALLED_APPS and USE_TINYMCE:
     from tinymce.widgets import TinyMCE
-    widget = TinyMCE(attrs={'cols': 80, 'rows': 30})
+    WIDGET = TinyMCE(attrs={'cols': 80, 'rows': 30})
 else:
-    widget = forms.Textarea()
+    WIDGET = forms.Textarea()
 
 class StaffMemberForm(forms.ModelForm):
-    bio = forms.CharField(widget=widget)
+    """
+    Basic form for entering data about the staff member. Changes the bio
+    field to a tinyMCE widget if specified.
+    """
+    bio = forms.CharField(widget=WIDGET)
+    
     class Meta:
         model = StaffMember
-        exclude = ('slug','first_name','last_name')
+        exclude = ('slug', 'first_name', 'last_name')
 
 
 class ContactForm(forms.Form):
+    """
+    A simple contact form to allow contacting the staff
+    """
     name = forms.CharField(label=_('Your Name'))
     email = forms.EmailField(label=_('Your Email'))
     message = forms.CharField(label=_('Message'), widget=forms.Textarea)
