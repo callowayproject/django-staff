8716504d8c86e2aff8a23d85127fc08d1976d245
Corey Oordt <coordt@washingtontimes.com>
2010-09-13 10:17:50 -0400

    Refactored the admin so it won't show up if the user isn't a staff member.

--- staff/admin.py
+++ staff/admin.py
@@ -1,11 +1,16 @@
-from models import StaffMember
 from django.contrib import admin
-from forms import StaffMemberForm
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
+from django import forms
+from django.utils.functional import curry
+from django.contrib.admin.util import flatten_fieldsets
+from django.forms.models import inlineformset_factory
+
+from staff.forms import StaffMemberForm
+from staff.models import StaffMember
 
 class StaffMemberAdmin(admin.StackedInline):
-    form = StaffMemberForm
+    # form = StaffMemberForm
     fieldsets = (
         ('Personal Info', {'fields': ('bio', 'photo', 'is_active', 'phone',)}),
         ('Responsibilities', {'fields': ('sites',)}),
@@ -13,6 +18,20 @@ class StaffMemberAdmin(admin.StackedInline):
     filter_horizontal = ('sites',)
     model = StaffMember
     max_num = 1
+    
+    def get_formset(self, request, obj=None, **kwargs):
+        """
+        Return a form, if the obj is_staff, otherwise return an empty form
+        """
+        if obj is not None and obj.is_staff:
+            return super(StaffMemberAdmin, self).get_formset(request, obj, **kwargs)
+        
+        defaults = {
+            "exclude": None,
+            "extra": 0,
+            "max_num": 0,
+        }
+        return inlineformset_factory(self.parent_model, self.model, **defaults)
 
 class StaffUserAdmin(UserAdmin):
     inlines = [StaffMemberAdmin,]
