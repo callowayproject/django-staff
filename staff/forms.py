4699d761a6ff01378e5c9cd9d23ed52be02dabe5
Corey Oordt <coordt@washingtontimes.com>
2010-09-13 10:16:18 -0400

    Removed the tinyMCE integration, to allow integration in other ways

--- staff/forms.py
+++ staff/forms.py
@@ -4,19 +4,12 @@ from django.conf import settings
 
 from models import StaffMember
 
-USE_TINYMCE = getattr(settings, 'STAFF_USE_TINYMCE', True)
-if 'tinymce' in settings.INSTALLED_APPS and USE_TINYMCE:
-    from tinymce.widgets import TinyMCE
-    WIDGET = TinyMCE(attrs={'cols': 80, 'rows': 30})
-else:
-    WIDGET = forms.Textarea()
-
 class StaffMemberForm(forms.ModelForm):
     """
     Basic form for entering data about the staff member. Changes the bio
     field to a tinyMCE widget if specified.
     """
-    bio = forms.CharField(widget=WIDGET)
+    # bio = forms.CharField(widget=WIDGET)
     
     class Meta:
         model = StaffMember
