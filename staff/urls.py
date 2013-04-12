b1289dc95d53b65bda65993d2cdbef3a069ae39b
Corey Oordt <coreyoordt@gmail.com>
2013-04-11 19:55:25 -0500

    Converted the generic urls to class-based urls

--- staff/urls.py
+++ staff/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView, DetailView
 from models import StaffMember
 
 STAFFMEMBER_INFO = {'model': StaffMember}
@@ -16,8 +16,7 @@ urlpatterns = patterns('staff.views',
         'contact',
         name='staff_staffmember_contact'),
     url(r'^contact/done/$',
-        direct_to_template,
-        {'template': 'staffmembers/contact_done.html'},
+        TemplateView.as_view(template_name='staffmembers/contact_done.html'),
         name='staff_staffmember_contact_done'),
     url(r'^(?P<slug>[^/]+)/stories/$',
         'story_archive',
@@ -26,7 +25,6 @@ urlpatterns = patterns('staff.views',
 
 urlpatterns += patterns('',
     url(r'^(?P<slug>[^/]+)/$',
-        'django.views.generic.list_detail.object_detail',
-        {'queryset': QUERYSET, 'template_name': 'staffmembers/details.html', },
+        DetailView.as_view(queryset=QUERYSET, template_name='staffmembers/details.html'),
         name='staff_staffmember_detail'),
 )
